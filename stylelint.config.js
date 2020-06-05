c1d66593d6a8e6d7526f4ff02a7f618aa25291ba
uimarshall <uimarshall@gmail.com>
2020-06-05 17:55:55 +0100

    Refactor code to reflect changes after review

--- stylelint.config.js
+++ stylelint.config.js
@@ -13,6 +13,21 @@ module.exports = {
 		"at-rule-name-case": "lower",
 		"at-rule-name-space-after": "always-single-line",
 		"at-rule-semicolon-newline-after": "always",
+		"at-rule-no-unknown": [
+			true,
+			{
+				ignoreAtRules: [
+					"each",
+					"extend",
+					"for",
+					"function",
+					"if",
+					"include",
+					"mixin",
+					"while",
+				],
+			},
+		],
 		"block-closing-brace-empty-line-before": "never",
 		"block-closing-brace-newline-after": "always",
 		"block-closing-brace-newline-before": "always-multi-line",
@@ -105,6 +120,5 @@ module.exports = {
 		"value-list-comma-space-after": "always-single-line",
 		"value-list-comma-space-before": "never",
 		"value-list-max-empty-lines": 0,
-		"no-descending-specificity": null,
 	},
 };
